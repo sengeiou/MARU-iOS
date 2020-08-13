c35036a7962267e6322a5a031b918e3921839ebb
junhyeon <oh.junhyeon95@gmail.com>
2020-08-14 01:04:15 +0900

    세팅

---
+++ MARU/Global/Utils/Utils.swift
@@ -0,0 +1,9 @@
+//
+//  Utils.swift
+//  MARU
+//
+//  Created by 오준현 on 2020/08/14.
+//  Copyright © 2020 maru. All rights reserved.
+//
+
+import Foundation
---
+++ MARU/Screens/Moim/ViewControllers/MoimVC.swift
@@ -0,0 +1,30 @@
+//
+//  MoimVC.swift
+//  MARU
+//
+//  Created by 오준현 on 2020/08/14.
+//  Copyright © 2020 maru. All rights reserved.
+//
+
+import UIKit
+
+class MoimVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
