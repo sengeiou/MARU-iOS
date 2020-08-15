b13205dddff38677f9b71f84cb7d791a470bb77f
yoonjinlee <yoonjinifer@gmail.com>
2020-08-16 04:04:31 +0900

    update: 검색뷰 위치잡기

---
+++ MARU/Screens/Search/ViewControllers/SearchResultVC.swift
@@ -0,0 +1,19 @@
+//
+//  SearchResultVC.swift
+//  MARU
+//
+//  Created by 이윤진 on 2020/08/16.
+//  Copyright © 2020 maru. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultVC: UIViewController{
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    
+    
+    
+}
---
+++ MARU/Screens/Search/ViewControllers/SearchVC.swift
@@ -0,0 +1,20 @@
+//
+//  SearchVC.swift
+//  MARU
+//
+//  Created by 이윤진 on 2020/08/16.
+//  Copyright © 2020 maru. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class SearchVC: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+}
