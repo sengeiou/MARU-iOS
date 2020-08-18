aadddfa8a02abde7d18a280a31962c00c7577e79
junhyeon <oh.junhyeon95@gmail.com>
2020-08-19 02:49:24 +0900

    update: 디버그시 뷰 오류 보기 위한 라이브러리 주입

--- MARU/Global/App/AppDelegate.swift
+++ MARU/Global/App/AppDelegate.swift
@@ -9,6 +9,10 @@
 import UIKit
 import CoreData
 
+#if DEBUG
+import Gedatsu
+#endif
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,6 +20,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        #if DEBUG
+        Gedatsu.open()
+        #endif
+
         return true
     }
 
