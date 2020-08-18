c72d44fad3364d502821e22e0df1c09acb17a881
Junhyeon <Oh.junhyeon95@gmail.com>
2020-08-19 03:16:18 +0900

    Feature/#3  (#7)
    * chor: 테스팅 json 파일 생성
    
    * create: cell indentifier 관리를 위한 enum 파일 생성
    
    * update: 디버그시 뷰 오류 보기 위한 라이브러리 주입
    
    * create: spinner 생성
    
    * chore: json 수정
    
    * update: 채팅 레이아웃 구성
    
    * update: 프로젝트 파일 수정을 위한 환경설정 수정
    
    * create: RxSwift 추가

--- MARU/Screens/Chat/Cell/ChatCVCell.swift
+++ MARU/Screens/Chat/Cell/ChatCVCell.swift
@@ -16,7 +16,7 @@ class ChatCVCell: UICollectionViewCell {
     
     let id = KeychainWrapper.standard.string(forKey: "id")
     var message: Message?
-
+    
     let nameLabel = UILabel().then {
         $0.font = Font.chatNameLabel
         $0.numberOfLines = 0
@@ -29,17 +29,17 @@ class ChatCVCell: UICollectionViewCell {
     
     let chatBubbleView = UIView().then {
         $0.backgroundColor = .white
-        $0.setRounded(radius: 22)
+        $0.setRounded(radius: 16)
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
-        print(#function)
+        
         nameLabel.text = nil
         chatLabel.text = nil
         chatBubbleView.backgroundColor = .white
         chatBubbleView.setBorder(borderColor: .black22, borderWidth: 0)
-        
+        chatBubbleView.frame = CGRect.init()
         updateLayout()
     }
     
@@ -47,73 +47,52 @@ class ChatCVCell: UICollectionViewCell {
         self.setNeedsLayout()
         self.layoutIfNeeded()
     }
-
+    
     
     func setConstraint() {
         contentView.addSubview(chatBubbleView)
         contentView.addSubview(chatLabel)
-        contentView.addSubview(nameLabel)
-
+        if message?.name != "" {
+            contentView.addSubview(nameLabel)
+            nameLabel.text = message?.name
+            nameLabel.sizeToFit()
+        }
+        
         chatLabel.text = message?.message
         chatLabel.sizeToFit()
-        nameLabel.text = message?.name
-        nameLabel.sizeToFit()
     }
     
-    func find() {
-        print(#function)
-        message?.name == id ? setMyChat() : setOtherChat()
-    }
-    
-    func setMyChat(){
-        print(#function, message?.name)
-        chatBubbleView.backgroundColor = .veryLightPinkTwo
-
-        nameLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(contentView.snp.top)
-            make.trailing.equalTo(contentView.snp.trailing).offset(-16)
-        }
-        
-        chatLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(nameLabel.snp.bottom).offset(16)
-            make.trailing.equalTo(contentView.snp.trailing).offset(-20)
-            make.width.lessThanOrEqualTo(contentView.snp.width).offset(-100)
-        }
-        
-        chatLabel.sizeToFit()
-        
-        chatBubbleView.snp.makeConstraints { (make) in
-            make.top.equalTo(chatLabel.snp.top).offset(-11)
-            make.leading.equalTo(chatLabel.snp.leading).offset(-13)
-            make.trailing.equalTo(chatLabel.snp.trailing).offset(13)
-            make.bottom.equalTo(chatLabel.snp.bottom).offset(11)
-        }
-    }
     
     func setOtherChat(){
-        print(#function, message?.name)
         chatBubbleView.setBorder(borderColor: .black22, borderWidth: 1)
         chatBubbleView.backgroundColor = .white
-
-        nameLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(contentView.snp.top)
-            make.leading.equalTo(contentView.snp.leading).offset(16)
-        }
         
-        chatLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(nameLabel.snp.bottom).offset(16)
-            make.leading.equalTo(contentView.snp.leading).offset(20)
-            make.width.lessThanOrEqualTo(contentView.snp.width).offset(-100)
+        if message?.name == "" {
+            chatLabel.snp.makeConstraints { (make) in
+                make.top.equalTo(contentView.snp.top).offset(11)
+                make.leading.equalTo(contentView.snp.leading).offset(20)
+                make.width.lessThanOrEqualTo(contentView.snp.width).offset(-100)
+            }
+        } else {
+            nameLabel.snp.makeConstraints { (make) in
+                make.top.equalTo(contentView.snp.top)
+                make.leading.equalTo(contentView.snp.leading).offset(16)
+            }
+            chatLabel.snp.makeConstraints { (make) in
+                make.top.equalTo(nameLabel.snp.bottom).offset(16)
+                make.leading.equalTo(contentView.snp.leading).offset(20)
+                make.width.lessThanOrEqualTo(contentView.snp.width).offset(-100)
+            }
         }
         
         chatLabel.sizeToFit()
-
+        
         chatBubbleView.snp.makeConstraints { (make) in
             make.top.equalTo(chatLabel.snp.top).offset(-11)
             make.leading.equalTo(chatLabel.snp.leading).offset(-13)
             make.trailing.equalTo(chatLabel.snp.trailing).offset(13)
             make.bottom.equalTo(chatLabel.snp.bottom).offset(11)
         }
-
+        
     }
 }
