c7bfb650c0fc6ef64bc10f3221f322c6b0f62380
junhyeon <oh.junhyeon95@gmail.com>
2020-08-19 02:50:32 +0900

    update: 채팅 레이아웃 구성

--- MARU/Screens/Chat/Cell/MyChatCVCell.swift
+++ MARU/Screens/Chat/Cell/MyChatCVCell.swift
@@ -1,8 +1,8 @@
 //
-//  ChatCVCell.swift
+//  MyChatCVCell.swift
 //  MARU
 //
-//  Created by 오준현 on 2020/08/15.
+//  Created by 오준현 on 2020/08/18.
 //  Copyright © 2020 maru. All rights reserved.
 //
 
@@ -12,15 +12,10 @@ import Then
 import SnapKit
 import SwiftKeychainWrapper
 
-class ChatCVCell: UICollectionViewCell {
+class MyChatCVCell: UICollectionViewCell {
     
     let id = KeychainWrapper.standard.string(forKey: "id")
     var message: Message?
-    
-    let nameLabel = UILabel().then {
-        $0.font = Font.chatNameLabel
-        $0.numberOfLines = 0
-    }
     
     let chatLabel = UILabel().then {
         $0.font = Font.chatLabel
@@ -34,8 +29,7 @@ class ChatCVCell: UICollectionViewCell {
     
     override func prepareForReuse() {
         super.prepareForReuse()
-        
-        nameLabel.text = nil
+
         chatLabel.text = nil
         chatBubbleView.backgroundColor = .white
         chatBubbleView.setBorder(borderColor: .black22, borderWidth: 0)
@@ -47,44 +41,28 @@ class ChatCVCell: UICollectionViewCell {
         self.setNeedsLayout()
         self.layoutIfNeeded()
     }
-    
+
     
     func setConstraint() {
         contentView.addSubview(chatBubbleView)
         contentView.addSubview(chatLabel)
-        if message?.name != "" {
-            contentView.addSubview(nameLabel)
-            nameLabel.text = message?.name
-            nameLabel.sizeToFit()
-        }
-        
+
         chatLabel.text = message?.message
         chatLabel.sizeToFit()
     }
     
+    func find() {
+        setMyChat()
+    }
     
-    func setOtherChat(){
-        chatBubbleView.setBorder(borderColor: .black22, borderWidth: 1)
-        chatBubbleView.backgroundColor = .white
+    func setMyChat(){
+        chatBubbleView.backgroundColor = .veryLightPinkTwo
         
-        if message?.name == "" {
-            chatLabel.snp.makeConstraints { (make) in
-                make.top.equalTo(contentView.snp.top).offset(11)
-                make.leading.equalTo(contentView.snp.leading).offset(20)
-                make.width.lessThanOrEqualTo(contentView.snp.width).offset(-100)
-            }
-        } else {
-            nameLabel.snp.makeConstraints { (make) in
-                make.top.equalTo(contentView.snp.top)
-                make.leading.equalTo(contentView.snp.leading).offset(16)
-            }
-            chatLabel.snp.makeConstraints { (make) in
-                make.top.equalTo(nameLabel.snp.bottom).offset(16)
-                make.leading.equalTo(contentView.snp.leading).offset(20)
-                make.width.lessThanOrEqualTo(contentView.snp.width).offset(-100)
-            }
+        chatLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(contentView.snp.top).offset(11)
+            make.trailing.equalTo(contentView.snp.trailing).offset(-20)
+            make.width.lessThanOrEqualTo(contentView.snp.width).offset(-100)
         }
-        
         chatLabel.sizeToFit()
         
         chatBubbleView.snp.makeConstraints { (make) in
@@ -93,6 +71,6 @@ class ChatCVCell: UICollectionViewCell {
             make.trailing.equalTo(chatLabel.snp.trailing).offset(13)
             make.bottom.equalTo(chatLabel.snp.bottom).offset(11)
         }
-        
     }
+    
 }
