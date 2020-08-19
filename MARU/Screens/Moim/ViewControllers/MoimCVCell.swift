6385187263319bd9a7a7648cce08b23a1f1188b9
juyeonblue <jyp0211@ajou.ac.kr>
2020-08-20 02:02:05 +0900

    [First Commit] 모임 뷰

---
+++ MARU/Screens/Moim/ViewControllers/MoimCVCell.swift
@@ -0,0 +1,26 @@
+//
+//  MoimCVCell.swift
+//  MARU
+//
+//  Created by 박주연 on 2020/08/20.
+//  Copyright © 2020 maru. All rights reserved.
+//
+
+import UIKit
+
+class MoimCVCell: UICollectionViewCell {
+    
+    @IBOutlet weak var bookImg: UIImageView!
+    @IBOutlet weak var bookFrontImg: UIImageView!
+    
+    @IBOutlet weak var bookNameLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var countTimeLabel: UILabel!
+    @IBOutlet weak var bookIntroduceLabel: UILabel!
+    
+    @IBOutlet weak var chatNickNameLabel: UILabel!
+    @IBOutlet weak var chatTimeLabel: UILabel!
+    @IBOutlet weak var chattingLabel: UILabel!
+    @IBOutlet weak var chatCountLabel: UILabel!
+    
+}
--- MARU/Screens/Moim/ViewControllers/MoimVC.swift
+++ MARU/Screens/Moim/ViewControllers/MoimVC.swift
@@ -10,21 +10,14 @@ import UIKit
 
 class MoimVC: UIViewController {
 
+    @IBOutlet weak var moimCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+      
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
