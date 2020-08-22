3c84b273a6673526fd5f9f606ce877883c56bbbf
juyeonblue <jyp0211@ajou.ac.kr>
2020-08-23 03:31:45 +0900

    [MoimView] PageControl 추가

--- MARU/Screens/Moim/ViewControllers/MoimVC.swift
+++ MARU/Screens/Moim/ViewControllers/MoimVC.swift
@@ -6,30 +6,41 @@
 //  Copyright © 2020 maru. All rights reserved.
 //
 
+import CHIPageControl
 import UIKit
 
 class MoimVC: UIViewController {
     
     @IBOutlet weak var moimCollectionView: UICollectionView!
+    @IBOutlet weak var paging: CHIPageControlFresno!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         moimCollectionView.delegate = self
         moimCollectionView.dataSource = self
-     
+        
+        pageControlSize()
     }
     
     
+    func pageControlSize() {
+        paging.numberOfPages = 3
+        paging.radius = 4
+        paging.tintColor = UIColor(red: 194, green: 194, blue: 194)
+        paging.currentPageTintColor = UIColor(red: 194, green: 194, blue: 194)
+        paging.padding = 6
+//        paging.insertTintColor(UIColor(red: 194, green: 194, blue: 194), position: 2)
+
+    }
     
 }
 
 extension MoimVC: UICollectionViewDelegate{
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-   
+        
     }
-    
 }
 
 extension MoimVC: UICollectionViewDataSource {
