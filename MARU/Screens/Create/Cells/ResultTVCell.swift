50cc98083328e180d820e71afad15aaefede4b3e
yoonjinlee <yoonjinifer@gmail.com>
2020-08-24 22:16:01 +0900

    create: 검색파일/검색기록파일 생성

---
+++ MARU/Screens/Create/Cells/ResultTVCell.swift
@@ -0,0 +1,24 @@
+//
+//  ResultTVCell.swift
+//  MARU
+//
+//  Created by 이윤진 on 2020/08/24.
+//  Copyright © 2020 maru. All rights reserved.
+//
+
+import UIKit
+
+class ResultTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ MARU/Screens/Create/ViewControllers/ResultVC.swift
@@ -0,0 +1,16 @@
+//
+//  ResultVC.swift
+//  MARU
+//
+//  Created by 이윤진 on 2020/08/24.
+//  Copyright © 2020 maru. All rights reserved.
+//
+
+import UIKit
+
+class ResultVC: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
