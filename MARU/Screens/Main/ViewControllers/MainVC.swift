1e533dcba50186f7fa525b850573af9d14123395
KIN <psychehose@gmail.com>
2020-08-26 02:33:48 +0900

    create:  MainView HeaderView close #30

--- MARU/Screens/Main/ViewControllers/MainVC.swift
+++ MARU/Screens/Main/ViewControllers/MainVC.swift
@@ -14,174 +14,107 @@ import SnapKit
 class MainVC: UIViewController, UITextFieldDelegate {
     
     
-    //MARK: - UI Components ( CollectionView )
-    
-    let popularMeetingColletionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        
-        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        cv.translatesAutoresizingMaskIntoConstraints = false
-        cv.register(PopularMeetingCVC.self, forCellWithReuseIdentifier: "PopularMeetingCVC")
-        
-        cv.backgroundColor = . red
-        cv.showsHorizontalScrollIndicator = false
-        return cv
-    }()
+    //MARK: - UI Components
     
     let newMeetingCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
+        layout.sectionInset  = .zero
+        layout.sectionInsetReference = .fromContentInset
         
         
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
         cv.backgroundColor = .white
         cv.translatesAutoresizingMaskIntoConstraints = false
+        
         cv.register(NewMeetingCVC.self, forCellWithReuseIdentifier: "NewMeetingCVC")
+        cv.register(HeaderVC.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier:"HeaderVC")
         return cv
     }()
     
-    //MARK: - UI Components ( About BackImgView )
-    
-    let backImageView = UIImageView().then {
-        $0.image = UIImage(named: "picture")
-    }
-    
-    let backImageViewScrim = UIView().then {
-        $0.backgroundColor = UIColor.black2
-        $0.alpha = 0.5
-    }
-    let bookLogoImageView = UIImageView().then {
-        $0.image = UIImage(named: "bookImgWhite")
-    }
-    let commentLabel = UILabel().then {
-        $0.numberOfLines = 2
-        $0.font = UIFont.systemFont(ofSize: 20, weight: UIFont.Weight.bold)
-        $0.textColor = .white
-        $0.text = "사람들과 함께\n책장을 넘겨보세요."
-    }
-    
-    let searchLabel = UIView().then {
-        $0.backgroundColor = .white
-        $0.layer.borderWidth = 1
-        $0.layer.cornerRadius = 10
-        $0.layer.borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0).cgColor
-        $0.layer.masksToBounds = true
-        $0.layer.shadowColor = UIColor.black.cgColor
-
-        $0.layer.shadowOffset = .zero
-        $0.layer.shadowRadius = 1.5
-        $0.layer.shadowOpacity = 0.28
-        $0.layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
-    }
-    
-    let searchImage = UIImageView().then {
-        $0.image = UIImage(named: "search")
-    }
-    let searchTextField = UITextField().then {
-        $0.text = ""
-        $0.placeholder  = "책 제목을 입력해주세요."
-        $0.tintColor = .black
-        $0.textAlignment = .left
-        $0.font = UIFont.systemFont(ofSize: 13, weight: UIFont.Weight.bold)
-    }
-    
-    let mypageButton = UILabel().then {
-        $0.font = UIFont.systemFont(ofSize: 12, weight: UIFont.Weight.bold)
-        $0.textColor = .white
-        $0.text = "MY"
-    }
-    let popularMeetingLabel = UILabel().then {
-        $0.font = UIFont.systemFont(ofSize: 18, weight: UIFont.Weight.bold)
-        $0.text = "지금 가장 모임이 많은 책은?"
-    }
-    let newMeetingLabel = UILabel().then {
-        $0.font = UIFont.systemFont(ofSize: 18, weight: UIFont.Weight.bold)
-        $0.text = "지금 새로 나온 모임"
-    }
-    
-    let contentView = UIView()
+        let contentView = UIView()
     
     //MARK: - viewDidLoad
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        searchTextField.delegate = self
-        layout()
-        keyboardNotification()
         
+        layout()
     }
-    
     //MARK: - Method
-    
-    
-    
     
 }
 extension MainVC: UICollectionViewDelegateFlowLayout {
+    
+    
+    
+    
+    //MARK: - Header View Size
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        
+        return CGSize(width: collectionView.frame.width, height: self.view.frame.height * (0.689))
+    }
+
     
     //MARK: - CellCize
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        switch collectionView {
-            
-        case self.popularMeetingColletionView:
-            return CGSize(width: 81, height: 170)
-            
-        case self.newMeetingCollectionView:
-            return CGSize(width: self.view.frame.width * (343/375) , height: 110)
-        default:
-            return CGSize(width: 0, height: 0)
-        }
+        return CGSize(width: self.view.frame.width * (343/375) , height: 110)
     }
     //MARK: - CollectionView ContentView Inset
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         
-        switch collectionView {
-        case self.popularMeetingColletionView:
-            return UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
-            
-        case self.newMeetingCollectionView:
-            return UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
-        default:
-            return UIEdgeInsets()
-        }
+        return UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        
     }
+    
+    
+    
+    
     //MARK: - distance between Cells
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        
         return 23
-    }
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 15
     }
 }
 
 extension MainVC: UICollectionViewDataSource {
     
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
     }
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        switch collectionView {
-        case self.popularMeetingColletionView:
-            guard let popularCell = collectionView.dequeueReusableCell(withReuseIdentifier: "PopularMeetingCVC", for: indexPath) as? PopularMeetingCVC else { return UICollectionViewCell()}
-            return popularCell
-            
-        case self.newMeetingCollectionView:
-            guard let newMeetingCell = collectionView.dequeueReusableCell(withReuseIdentifier: "NewMeetingCVC", for: indexPath) as? NewMeetingCVC else { return UICollectionViewCell()}
-            return newMeetingCell
-            
-        default:
-            return UICollectionViewCell()
-        }
+        guard let newMeetingCell = collectionView.dequeueReusableCell(withReuseIdentifier: "NewMeetingCVC", for: indexPath) as? NewMeetingCVC else { return UICollectionViewCell()}
+        return newMeetingCell
+    }
+    
+    //MARK: - HeaderView reusable
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) ->
+        UICollectionReusableView {
+            switch kind {
+            case UICollectionView.elementKindSectionHeader:
+                let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "HeaderVC", for: indexPath) as? HeaderVC
+                return headerView ?? UICollectionReusableView()
+            default:
+                assert(false, "false")
+            }
     }
     
 }
 
-    //MARK: - Keyboard setting
+//MARK: - Keyboard setting
 
 extension MainVC {
     
@@ -195,7 +128,7 @@ extension MainVC {
     }
     
     @objc func keyboardWillShow(_ sender: Notification) {
-        }
+    }
     @objc func keyboardWillHide(_ sender: Notification) {
     }
     
